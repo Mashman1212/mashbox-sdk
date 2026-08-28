cd9ae2024ffcd28bc479602a9ae0cc5995f41302
Matthew Jensen <mashgames12@outlook.com>
2026-08-27 21:42:01 -0700

    u

--- com.mg.mashbox.sdk/Editor/Exporting/GameRegistry.cs
+++ com.mg.mashbox.sdk/Editor/Exporting/GameRegistry.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 namespace MashBoxSDK.Exporting
@@ -103,6 +105,7 @@ namespace MashBoxSDK.Exporting
         }
     }
 
+#if UNITY_EDITOR
     public static class GameTargetPublishWarning
     {
         public static bool ConfirmIfGameIsNotInstalled(string gameName)
@@ -120,4 +123,5 @@ namespace MashBoxSDK.Exporting
                 "Cancel");
         }
     }
+#endif
 }
